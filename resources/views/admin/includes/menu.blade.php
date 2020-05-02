253ced7a147498a3f6305b4ff46bb184dafe9ff1
obuidulpias <obuidul.pias@gmail.com>
2020-05-03 00:16:58 +0600

    update category

--- resources/views/admin/includes/menu.blade.php
+++ resources/views/admin/includes/menu.blade.php
@@ -13,7 +13,7 @@
 
   <!-- Nav Item - Dashboard -->
   <li class="nav-item active">
-    <a class="nav-link" href="index.html">
+    <a class="nav-link" href="{{ Route('home') }}">
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Dashboard</span></a>
   </li>
@@ -23,20 +23,19 @@
 
   <!-- Heading -->
   <div class="sidebar-heading">
-    Interface
+    GO AHEAD
   </div>
 
   <!-- Nav Item - Pages Collapse Menu -->
   <li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-      <i class="fas fa-fw fa-cog"></i>
-      <span>Components</span>
+      <i class="fas fa-fw fa-chart-area"></i>
+      <span>Category</span>
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Custom Components:</h6>
-        <a class="collapse-item" href="buttons.html">Buttons</a>
-        <a class="collapse-item" href="cards.html">Cards</a>
+        <a class="collapse-item" href="{{ Route('add-category') }}">Add Category</a>
+        <a class="collapse-item" href="{{ Route('manage-category') }}">Manage Category</a>
       </div>
     </div>
   </li>
